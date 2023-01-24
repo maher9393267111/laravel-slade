7d012906dd15ba0df997d82ec81194927bdc3563
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-01-24 16:15:01 +0300

    add

---
+++ app/Http/Requests/PostStoreRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class PostStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+
+            //
+            'category_id' => 'required|exists:categories,id',
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'slug' => 'required|max:255', Rule::unique('posts', 'slug')->ignore($this->route('post'))
+        ];
+    }
+}
